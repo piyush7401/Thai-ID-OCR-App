b37445877861a55188f16e4a6da608feef2db4cf
Piyush Varshney <piyush07varshney@gmail.com>
2023-12-24 18:51:36 +0530

    Connection of Momgodb

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const mongoose = require("mongoose")
 const express = require("express")
 const app = express()
+const ocrRoutes = require('./routes/ocrRoutes')
 const port = 8000;
+
+// Connection of mongodb
+mongoose.connect('your-mongodb-connection-string', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// middlewares 
+app.use(express.json());
+
+// routes 
+app.use('/api', ocrRoutes);
 
 
 app.listen(port,(req,res) =>{
