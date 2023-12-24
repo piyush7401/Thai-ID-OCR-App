852d2d6ab8b0b71d43b087afb28f9a1ca71f7191
Piyush Varshney <piyush07varshney@gmail.com>
2023-12-24 19:11:10 +0530

    Create a controller of create_ocr_record

---
+++ controllers/ocr_controller.js
@@ -0,0 +1,48 @@
+const OCRRecord = require('../models/ocr_record');
+
+async function createOCRRecord(req,res){
+    try {
+        const body = req.body;
+      
+        // Check if the required fields are present in the request body
+        if (!body || !body.identification_number || !body.name || !body.last_name || !body.date_of_birth || !body.date_of_issue || !body.date_of_expiry) {
+          return res.status(400).json({ error: 'Incomplete or missing data in the request body.' });
+        }
+      
+        // Implement OCR processing logic here...
+      
+        // Use data from req.body instead of hardcoded values
+        const ocrData = {
+          identification_number: body.identification_number,
+          name: body.name,
+          last_name: body.last_name,
+          date_of_birth: body.date_of_birth,
+          date_of_issue: body.date_of_issue,
+          date_of_expiry: body.date_of_expiry,
+          status: 'success', // or 'failure'
+          error_message: '...', // Provide an error message if status is 'failure'
+        };
+      
+        // Create a new OCR record and save it to the database
+        const newOCRRecord = await OCRRecord.create(ocrData);
+      
+        res.json({
+          identification_number: newOCRRecord.identification_number,
+          name: newOCRRecord.name,
+          last_name: newOCRRecord.last_name,
+          date_of_birth: newOCRRecord.date_of_birth,
+          date_of_issue: newOCRRecord.date_of_issue,
+          date_of_expiry: newOCRRecord.date_of_expiry,
+          status: newOCRRecord.status,
+          error_message: newOCRRecord.error_message,
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+
+
+module.exports = {createOCRRecord};
