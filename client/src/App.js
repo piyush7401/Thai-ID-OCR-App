ae909049d2913e4611d0019579a9e08ad10b13c2
Piyush Varshney <piyush07varshney@gmail.com>
2023-12-25 14:30:32 +0530

    mmaking the forntend part

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [file, setFile] = useState(null);
+  const [ocrData, setOcrData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    setError(null);
+    setOcrData(null);
+
+    const formData = new FormData();
+    formData.append('idCardImage', file);
+
+    try {
+      const response = await axios.post('http://localhost:8000/api/ocr/create', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      setOcrData(response.data);
+    } catch (err) {
+      setError('Error uploading the file');
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style={{ textAlign: 'center', margin: '20px' }}>
+      <h1>OCR Frontend</h1>
+
+      <div style={{ margin: '10px' }}>
+        <label htmlFor="file">Select a Thai ID Card Image:</label>
+        <input type="file" id="file" onChange={handleFileChange} />
+      </div>
+
+      <button
+      style={{
+        backgroundColor: '#4CAF50',
+        color: 'white',
+        padding: '10px 15px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+      }} 
+      onClick={handleUpload}>Upload and Process</button>
+
+      {ocrData && (
+        <div style={{ margin: '20px' }}>
+          <h2>OCR Result:</h2>
+          <pre style={{ textAlign: 'left', background: '#f8f8f8', padding: '10px', borderRadius: '5px' }}>{JSON.stringify(ocrData, null, 2)}</pre>
+        </div>
+      )}
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
-}
+};
 
 export default App;
